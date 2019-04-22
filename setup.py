1dd62f2aa7ff602811228de2554d4d9033d2b754
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-22 14:01:57 +0100

    correctly load logging.conf from manifest. Version bump 0.0.91

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='subscribiecli',
-    version="0.0.9",
+    version="0.0.91",
     author="Karma Computing",
     author_email="subscribie@karmacomputing.co.uk",
     desciption="cli utility for Subscribie",
--- subscribie_cli/__init__.py
+++ subscribie_cli/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
-from os import path
+import os
 import logging
 import logging.config
-log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
+log_file_path = open(os.path.join(os.path.dirname(__file__), '../logging.conf'))
 logging.config.fileConfig(log_file_path)
 # Create logger
 logger = logging.getLogger('subscribiecli')
