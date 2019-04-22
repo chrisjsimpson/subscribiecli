1f4c2e5e6fae834b1e55907e79a4aa45658b39d7
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-22 15:08:03 +0100

    version bump 0.0.93

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='subscribiecli',
-    version="0.0.92",
+    version="0.0.93",
     author="Karma Computing",
     author_email="subscribie@karmacomputing.co.uk",
     desciption="cli utility for Subscribie",
