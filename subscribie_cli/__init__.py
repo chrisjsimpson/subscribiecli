296ae2f4387982ade0c4a4add2f02bd75e7eadde
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-22 14:52:30 +0100

    forget manifest, just add logging.conf inside package

--- subscribie_cli/__init__.py
+++ subscribie_cli/__init__.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import
 import os
 import logging
 import logging.config
-log_file_path = open(os.path.join(os.path.dirname(__file__), '../logging.conf'))
+this_dir, this_filename = os.path.split(__file__)
+DATA_PATH = os.path.join(this_dir, "logging.conf")
+log_file_path = open(DATA_PATH)
 logging.config.fileConfig(log_file_path)
 # Create logger
 logger = logging.getLogger('subscribiecli')
